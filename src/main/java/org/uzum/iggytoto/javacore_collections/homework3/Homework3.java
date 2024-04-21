04b1bf7e67e963b61600258dd0690e6208713a2d
Aleksey Zhuravlyov <iggytoto@gmail.com>
2024-04-21 10:57:10 +0300

    Examples: Collections and homework3

---
+++ src/main/java/org/uzum/iggytoto/javacore_collections/homework3/Homework3.java
@@ -0,0 +1,20 @@
+package org.uzum.iggytoto.javacore_collections.homework3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Класс для реализации домашнего задания номер три.
+ */
+public class Homework3 {
+
+    /**
+     * Реализовать метод который считает количество слов встречающихся в заданном тексте.
+     * @param text - заданный текст
+     * @return - словарь слов и количество их появлений в данном тексте
+     */
+    public Map<String, Integer> countWords(String text) {
+        /// Тут надо писать код связанный с домашним заданием #3
+        return new HashMap<>();
+    }
+}
