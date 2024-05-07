64b7d7cafcc2ba38640ba4564eefe4f2b32e26a6
Aleksey Zhuravlyov <iggytoto@gmail.com>
2024-05-07 09:55:40 +0300

    homework 6 added

---
+++ src/main/java/org/uzum/iggytoto/java_algorithms/Homework6_2.java
@@ -0,0 +1,39 @@
+package org.uzum.iggytoto.java_algorithms;
+
+/**
+ * You are given an array of integers nums. Return the length of the longest subarray of nums which is either
+ * strictly increasing or strictly decreasing.
+ * <p>
+ * Example 1:
+ * Input: nums = [1,4,3,3,2]
+ * Output: 2
+ * Explanation:
+ * The strictly increasing subarrays of nums are [1], [2], [3], [3], [4], and [1,4].
+ * The strictly decreasing subarrays of nums are [1], [2], [3], [3], [4], [3,2], and [4,3].
+ * Hence, we return 2.
+ * <p>
+ * Example 2:
+ * Input: nums = [3,3,3,3]
+ * Output: 1
+ * Explanation:
+ * The strictly increasing subarrays of nums are [3], [3], [3], and [3].
+ * The strictly decreasing subarrays of nums are [3], [3], [3], and [3].
+ * Hence, we return 1.
+ * <p>
+ * Example 3:
+ * Input: nums = [3,2,1]
+ * Output: 3
+ * Explanation:
+ * The strictly increasing subarrays of nums are [3], [2], and [1].
+ * The strictly decreasing subarrays of nums are [3], [2], [1], [3,2], [2,1], and [3,2,1].
+ * Hence, we return 3.
+ * <p>
+ * Constraints:
+ * 1 <= nums.length <= 50
+ * 1 <= nums[i] <= 50
+ */
+public class Homework6_2 {
+    public int longestMonotonicSubarray(int[] nums) {
+        return -1;
+    }
+}
